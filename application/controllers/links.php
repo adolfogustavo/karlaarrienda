d6ae19873444d93b1648f3406afac2dddf31a12f
Gustavo Dominguez <gustavodominguez230@gmail.com>
2019-02-03 17:37:29 -0300

    Agregando primera vista

--- application/controllers/links.php
+++ application/controllers/links.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class links extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,9 +18,9 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+     public function esr()
+     {
+          $this->load->view('link_1');
+     }
 
 }
